2f631aa70d0c8fedb25483f2f06468a1854ab7c0
Michael Kees <michaelkees@MacBook-Pro.local>
2021-03-12 10:11:15 +0100

    changed main branch

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express(); // Run the app by serving the static files
+const port = process.env.PORT || 8080;
+// in the dist directory
+app.use(express.static(__dirname + "/dist/seo-basic"));
+// Start the app by listening on the default
+// Heroku port
+app.listen(port);
+
+console.log("Running on port: " + port);
